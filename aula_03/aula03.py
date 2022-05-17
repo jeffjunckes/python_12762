9f2547758692eb55ae4c2c9a4d532ef9b97775f5
jeffjunckes <jeffjunckes2015@gmail.com>
2022-05-16 21:56:12 -0300

    16-05-2022

--- aula_03/aula03.py
+++ aula_03/aula03.py
@@ -1,19 +1,71 @@
 #strings
-string2 = ("outro exemplo de string")
-string3 = """este é 
-mais um exemplo de string"""
+#string2 = ("outro exemplo de string")
+#string3 = """este é 
+##mais um exemplo de string"""
+#htmlPaginaInicial = """
+#<html>
+#    <head>
+#    </head>
+#    <body>
+#        <hr />
+ #       <br />
+ #   </body>
+#</html>
+#"""
+#htmlCoisasParaEstudar = 
+#"""
+#<html>
+ #   <head>
+ #   </head>
+ #   <body>
+  #      <p>Padroes de nomenclatura para variaveis /p> 
+  #      <p>clean code</p>
+   #     <p> Pep 8 </p>#recomendações de como escrever codigo em python.
+        
+  #  </body>
+#</html>
+#"""
+#sString1 = "exemplo de string"
+#print(string2)
+##print(string3)
+#print(sString1)
+#print(sString1.upper())
+#print(sString1.title())
+##print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+#print("novo inicio a partir da aula")
+#print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
+#print("")
+#print("")
 
-sString1 = "exemplo de string"
-print(string2)
-print(string3)
+
+"""sString1 = ("acessando elementos de uma string".title())#deixa a primeira palavra de cada string maiuscula
 print(sString1)
-print(sString1.upper())
-print(sString1.title())
-print("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx")
-print("novo inicio a partir da aula")
-print("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX")
-print("")
-print("")
+print(sString1[10]) #pega o 10 elemento
+print(sString1[:10])#escreve ate a 10 letra
+print(sString1[10:])#escreve a partir da 10 letra sempre começando com o 0"""
+#prompt é a pergunta que ela vai te fazer.
+"""snome = input("digite seu nome: ") #comando imput é uma funçao do pyton para ler dados do teclado via terminal de comando .
+sEmail = input("digite seu email: ")
+sMensagem = "seja bem vindo {: ^20}, enviamos um email para endereço de email {} confirmando seu cadastro.".format(snome,sEmail )
+print(sMensagem)"""
+#csv -> comma separated values
+#as contra- barras serve para mostrar que a proxima aspas faz parte da string
+"""csvProduto = "01; televisor 32\"; marca samsung; preço: 1200,00"
+print(csvProduto)
+print(csvProduto.split(";"))
+lstProduto = csvProduto.split(";")
+print(lstProduto[1])
+print(";".join(lstProduto)) #metodo join concatena qualquer numero de strings"""
+
+
+
+
+
+
+
+
+
+
 
 
 
