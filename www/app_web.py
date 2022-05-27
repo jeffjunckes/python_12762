7ae770116e1e94080488a0d86a0bb762046fcaa2
jeffjunckes <jeffjunckes2015@gmail.com>
2022-05-26 21:58:32 -0300

    26-05

--- www/app_web.py
+++ www/app_web.py
@@ -1,7 +1,7 @@
 import cherrypy
 import os
 #criando um modelo da app web.
-class AppWeb(object):
+class AppWeb: #object não é um parametro.
     @cherrypy.expose # permite que você acesse o metodo a partir de um navegador
     def index(self):#self faz referenca ao proprio objeto da classe(ele falando dele mesmo)
         texto = "olá 123"
@@ -41,14 +41,41 @@ class AppWeb(object):
 #para colocar um ID dentro do marcador dea bertura colocamos is = "divPrincipal"
     @cherrypy.expose
     def formLogin(self):
-        return "formulario de login: "
+        return  """"<!DOCTYPE html>
+        <html>
+            <head>
+                <title>Python 12762</title>
+                <style>
+                    p{
+                        color: darkred;
+                    }
+                    p{
+                        
+                        }
+                    div{
+                        backgound-color:lightblue;
+                    }
+                    #divformLogin{
+                        backgound-color: rgb(200, 255, 200);
+                    }
+                </style>
+            </head>
+            <body>
+                <div id="divPrincipal>
+                <p> Curso programação em python -modulo web </p>
+    
+                    <div id="divformLogin">
+                        
+                    </div>
+                </div>
+            </body>
+        </html>"""
     def listaDeArquivos(self):
         lstArquivos = os.listdir()
         sListaDeArquivos = "lista de arquivos"
         return sListaDeArquivos
     def getHTMLHeader():
         return ""
-
 
 cherrypy.quickstart(AppWeb()) #metodo que vai criar a app web, ele vai criar nosso objeto da classe app web
 #
